89deab4c2e3fa35a2c4b02c1a9b79ea3f5aaa82c
jwd222 <sji.2.sami@gmail.com>
2023-07-26 17:43:29 +0600

    password not sent back in response body

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,16 @@ const register = async (req, res) => {
 
   const user = await User.create({ name, email, password })
   const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({ user, token })
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  })
 }
 const login = async (req, res) => {
   res.send('login user')
