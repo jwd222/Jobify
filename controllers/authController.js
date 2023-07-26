e4419b82269279fba94eee6251b9d4e0f2ad485d
jwd222 <sji.2.sami@gmail.com>
2023-07-26 16:54:14 +0600

    made custom error class to handle different errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,14 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
+import { BadRequestError } from '../errors/index.js'
 
 const register = async (req, res) => {
-  const user = await User.create(req.body)
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values')
+  }
+  const user = await User.create({ name, email, password })
   res.status(StatusCodes.CREATED).json({ user })
 }
 const login = async (req, res) => {
