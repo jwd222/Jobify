55f7a93387aa6733c0df5960b4b6997d4e95258a
jwd222 <sji.2.sami@gmail.com>
2023-07-26 06:34:54 +0600

    error handling with middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,13 @@
-const register = async (req, res) => {
-  res.send('register user')
+import errorHandlerMiddleware from '../middleware/error-handler.js'
+import User from '../models/User.js'
+
+const register = async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    res.status(201).json({ user })
+  } catch (error) {
+    next(error)
+  }
 }
 const login = async (req, res) => {
   res.send('login user')
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err)
-  res.status(500).json({ msg: 'there was an error' })
+  // console.log(err)
+  res.status(500).json({ msg: err })
 }
 export default errorHandlerMiddleware
