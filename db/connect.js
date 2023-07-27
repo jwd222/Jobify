f17d20e8776d2e350b7c1d924f59d35feda6fb07
jwd222 <sji.2.sami@gmail.com>
2023-07-27 20:18:24 +0600

    setup morgan for request logs

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,5 @@
 import mongoose from 'mongoose'
 
-const connectionString =
-  'mongodb+srv://shahjawad:<password>@nodeexpressprojects.lswuccc.mongodb.net/?retryWrites=true&w=majority'
-
 const connectDB = (url) => {
   return mongoose.connect(url)
 }
