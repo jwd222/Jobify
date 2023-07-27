e1809c20c51d00ebdb8d095302d312f4be51c332
jwd222 <sji.2.sami@gmail.com>
2023-07-27 21:12:25 +0600

    login backend complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,8 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError } from '../errors/index.js'
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js'
 
+// register controller
 const register = async (req, res) => {
   const { name, email, password } = req.body
 
@@ -26,9 +27,27 @@ const register = async (req, res) => {
     location: user.location,
   })
 }
+
+// login controller
 const login = async (req, res) => {
-  res.send('login user')
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values')
+  }
+  const user = await User.findOne({ email }).select('+password')
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  const token = user.createJWT()
+  user.password = undefined
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
+
 const updateUser = async (req, res) => {
   res.send('update user')
 }
