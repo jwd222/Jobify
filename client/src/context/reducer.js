1acb91e3e50f27e7ac53b0bfe816ae19046dc86c
jwd222 <sji.2.sami@gmail.com>
2023-07-29 05:03:36 +0600

    login functionality complete

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -60,6 +60,31 @@ const reducer = (state, action) => {
       alertText: action.payload.msg,
     }
   }
+  if (action.type === LOGIN_USER_BEGIN) {
+    return { ...state, isLoading: true }
+  }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      user: action.payload.user,
+      token: action.payload.token,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'login Successful! Redirecting...',
+    }
+  }
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    }
+  }
   throw new Error(`no such action: ${action}`)
 }
 export default reducer
