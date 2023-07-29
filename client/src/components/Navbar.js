abbf0a52c24d317348d90edefe9da2d876b41e1a
jwd222 <sji.2.sami@gmail.com>
2023-07-29 17:07:28 +0600

    navbar structure complete

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,40 @@
 import Wrapper from '../assets/wrappers/Navbar.js'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext.js'
+import Logo from './Logo.js'
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+
+        <div className="btn-container">
+          <button onClick={() => console.log('show logout')} className="btn">
+            <FaUserCircle />
+            sami
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              onClick={() => console.log('logout  user')}
+              className="dropdown-btn"
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
