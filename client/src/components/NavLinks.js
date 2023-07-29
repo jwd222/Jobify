34b074e657ef20d595919508a6194dd136325824
jwd222 <sji.2.sami@gmail.com>
2023-07-29 20:26:21 +0600

    set up navlinks component for both small and big sidebar

---
+++ client/src/components/NavLinks.js
@@ -0,0 +1,26 @@
+import links from '../utils/links.js'
+import { NavLink } from 'react-router-dom'
+
+const NavLinks = ({ toggleSidebar }) => {
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { text, path, id, icon } = link
+        return (
+          <NavLink
+            to={path}
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+            key={id}
+            onClick={toggleSidebar}
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        )
+      })}
+    </div>
+  )
+}
+export default NavLinks
