72894158ce6ea49b8e734e6be299ec21f927176e
jwd222 <sji.2.sami@gmail.com>
2023-08-06 06:19:59 +0600

    completed profile page and authentication on frontend

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -36,6 +36,14 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  // axios
+  const authFetch = axios.create({
+    baseURL: '/api/v1',
+    headers: {
+      Authorization: `Bearer ${state.token}`,
+    },
+  })
+
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT })
     clearAlert()
@@ -122,6 +130,16 @@ const AppProvider = ({ children }) => {
     dispatch({ type: LOGOUT_USER })
     removeUserFromLocalStorage()
   }
+
+  const updateUser = async (currentUser) => {
+    // console.log(currentUser)
+    try {
+      const { data } = await authFetch.patch('/auth/updateUser', currentUser)
+      console.log(data)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
   return (
     <AppContext.Provider
       value={{
@@ -132,6 +150,7 @@ const AppProvider = ({ children }) => {
         clearAlertWhenToggled,
         toggleSidebar,
         logoutUser,
+        updateUser,
       }}
     >
       {children}
--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,4 +1,68 @@
+import { useState } from 'react'
+import Wrapper from '../../assets/wrappers/DashboardFormPage.js'
+import { FormRow, Alert } from '../../components'
+import { useAppContext } from '../../context/appContext'
+
 const Profile = () => {
-  return <h1>Profile</h1>
+  const { user, showAlert, displayAlert, updateUser, isLoading } =
+    useAppContext()
+  const [name, setName] = useState(user?.name)
+  const [email, setEmail] = useState(user?.email)
+  const [lastName, setLastName] = useState(user?.lastName)
+  const [location, setLocation] = useState(user?.location)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // remove while testing
+    // if (!name || !email || !lastName || !location) {
+    //   displayAlert()
+    //   return
+    // }
+    updateUser({ name, email, lastName, location })
+  }
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        {showAlert && <Alert />}
+
+        {/* name */}
+        <div className="form-center">
+          <FormRow
+            labelText="Name"
+            type="text"
+            name="name"
+            value={name}
+            handleChange={(e) => setName(e.target.value)}
+          />
+          <FormRow
+            labelText="Last Name"
+            type="text"
+            name="lastName"
+            value={lastName}
+            handleChange={(e) => setLastName(e.target.value)}
+          />
+          <FormRow
+            labelText="Email"
+            type="email"
+            name="email"
+            value={email}
+            handleChange={(e) => setEmail(e.target.value)}
+          />
+          <FormRow
+            labelText="Location"
+            type="text"
+            name="location"
+            value={location}
+            handleChange={(e) => setLocation(e.target.value)}
+          />
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? 'Please wait...' : 'save changes'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  )
 }
 export default Profile
