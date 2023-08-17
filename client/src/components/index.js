9f2f3f5e8f3158f5c92fb5ec79e1e3685e3c5c03
jwd222 <sji.2.sami@gmail.com>
2023-08-18 05:10:28 +0600

    working on all jobs page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,5 +5,17 @@ import NavBar from './Navbar.js'
 import SmallSideBar from './SmallSideBar.js'
 import BigSideBar from './BigSideBar.js'
 import FormRowSelect from './FormRowSelect'
+import SearchContainer from './SearchContainer'
+import JobsContainer from './JobsContainer'
 
-export { Logo, FormRow, Alert, NavBar, SmallSideBar, BigSideBar, FormRowSelect }
+export {
+  Logo,
+  FormRow,
+  Alert,
+  NavBar,
+  SmallSideBar,
+  BigSideBar,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+}
