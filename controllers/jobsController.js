f5682ed4d130f0d601bf02bc75d15e44a73dad42
jwd222 <sji.2.sami@gmail.com>
2023-09-08 01:03:22 +0600

    charts set up complete on backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -7,6 +7,7 @@ import {
 } from '../errors/index.js'
 import checkPermissions from '../utils/checkPermissions.js'
 import mongoose from 'mongoose'
+import moment from 'moment'
 
 //
 const createJob = async (req, res) => {
@@ -85,7 +86,40 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0,
   }
 
-  let monthlyApplications = []
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: {
+          year: {
+            $year: '$createdAt',
+          },
+          month: {
+            $month: '$createdAt',
+          },
+        },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ])
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item
+
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format('MMM Y')
+
+      return { date, count }
+    })
+    .reverse()
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 }
