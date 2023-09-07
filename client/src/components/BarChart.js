74dd74ff72e60a36c309fe819a72484c808b5c4c
jwd222 <sji.2.sami@gmail.com>
2023-09-08 01:25:29 +0600

    barchart completed on frontend

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -1,8 +1,24 @@
-const BarChart = () => {
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
+
+const BarChartComponent = ({ data }) => {
   return (
-    <div>
-      <h1>BarChart</h1>
-    </div>
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
   )
 }
-export default BarChart
+export default BarChartComponent
