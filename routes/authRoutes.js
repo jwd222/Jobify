4d05cd2c7e883a853dfff0ad5bca695121547fc3
jwd222 <sji.2.sami@gmail.com>
2023-09-08 22:35:36 +0600

    added rate limiters and security packages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,8 +4,16 @@ const router = express.Router()
 import { register, login, updateUser } from '../controllers/authController.js'
 import authenticateUser from '../middleware/auth.js'
 
-router.route('/register').post(register)
-router.route('/login').post(login)
+import rateLimiter from 'express-rate-limit'
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+})
+
+router.route('/register').post(apiLimiter, register)
+router.route('/login').post(apiLimiter, login)
 router.route('/updateUser').patch(authenticateUser, updateUser)
 
 export default router
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+
 //db and authenticate user
 import connectDB from './db/connect.js'
 
@@ -27,7 +31,11 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.use(express.static(path.resolve(__dirname, './client/build')))
+
 app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 
 app.get('/', (req, res) => {
   res.json({ msg: 'welcome!' })
