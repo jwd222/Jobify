15fcc4019464da0eff376ee520c81204639a693f
jwd222 <sji.2.sami@gmail.com>
2023-09-08 20:47:13 +0600

    setting up pagination logic

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -68,11 +68,19 @@ const getAllJobs = async (req, res) => {
   if (sort === 'z-a') {
     result = result.sort('-position')
   }
+
+  // setup pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+  result = result.skip(skip).limit(limit)
+
   const jobs = await result
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs / limit)
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages })
 }
 //
 const updateJob = async (req, res) => {
